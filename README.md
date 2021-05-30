18c35608aef42591cc25ab2bcd5d1837cb83a0de
Grasso <eduardo.grasso@live.com>
2021-05-30 18:50:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-m8m1qf-evye45
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-m8m1qf-evye45)
---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent {
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
