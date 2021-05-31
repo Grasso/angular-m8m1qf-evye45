e89c7554226e467e99d902dded81c5ede7f8f97d
Grasso <eduardo.grasso@live.com>
2021-05-31 23:23:25 +0100

    3 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,6 +10,15 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
+  get total():number{
+    var t:number=0;
+    this.items.forEach(function(value){
+        t+=value.price;
+    });
+    return t;
+    
+  }
+
   constructor(private cartService: CartService) {
   
   
