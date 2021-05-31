dae56a23f0ee10fba94090c13204483bab8cb615
Grasso <eduardo.grasso@live.com>
2021-05-31 23:48:28 +0100

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -3,20 +3,36 @@ import { Product } from './products';
 
 
 @Injectable({ providedIn: 'root'   })
+
+//
+// CartService
+//
 export class CartService {
 
   constructor() { }
 
+  //
+  // items
+  //
   items: Product[] = [];
 
+  //
+  // addToCard
+  //
   addToCard(product:Product){
     this.items.push(product);
   }
 
+  //
+  // getItems
+  //
   getItems(){
     return this.items;
   }
 
+  //
+  // clearCart
+  //
   clearCart(){
     this.items = [];
     return this.items;
